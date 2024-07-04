47f31c66183127052847689ad4a76fa2544b14c0
doguebart <douglasweelber.2004@outlook.com>
2024-07-04 13:05:49 -0300

    feat: alter meals & users database tables

--- db/migrations/20240703213612_create_meals_table.ts
+++ db/migrations/20240703213612_create_meals_table.ts
@@ -3,15 +3,12 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("meals", (table) => {
     table.uuid("id").primary();
+    table.uuid("user_id").references("users.id").notNullable();
     table.text("name").notNullable();
     table.text("description").notNullable();
     table.boolean("isOnDiet").notNullable();
-    table.uuid("user_id").notNullable();
-    table
-      .foreign("user_id")
-      .references("id")
-      .inTable("users")
-      .onDelete("CASCADE");
+    table.date("date").notNullable();
+    table.timestamps(true, true);
   });
 }
 
--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -4,16 +4,21 @@ declare module "knex/types/tables" {
   export interface Tables {
     users: {
       id: string;
+      session_id: string;
       name: string;
-      session_id?: string;
+      email: string;
+      created_at: string;
+      updated_at: string;
     };
     meals: {
+      id: string;
+      user_id: string;
       name: string;
       description: string;
       isOnDiet: boolean;
+      date: number;
       created_at: string;
       updated_at: string;
-      user_id: string;
     };
   }
 }
