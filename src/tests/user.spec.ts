d002db93efcad24708b9f0d6105a405f56cfc807
doguebart <douglasweelber.2004@outlook.com>
2024-07-04 14:07:56 -0300

    test: Add create user test

---
+++ src/tests/user.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, beforeAll, afterAll, beforeEach, expect } from "vitest";
+import { app } from "../app";
+import request from "supertest";
+import { execSync } from "node:child_process";
+
+describe("Create User test", async () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
+  });
+
+  it("Should be able to create a new user.", async () => {
+    await request(app.server)
+      .post("/users")
+      .send({
+        name: "John Doe",
+        email: "johndoe@example.com",
+      })
+      .expect(201);
+  });
+});
